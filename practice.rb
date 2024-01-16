2e02ba277c15ea295cc072be91a7c46c1a44d19d
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2024-01-16 19:45:58 +0900

    例文コード追加

--- practice.rb
+++ practice.rb
@@ -4,3 +4,24 @@ while a <= 10
   puts a
   a = a + 1
 end
+
+#10から１までの整数を順番に表示する
+a = 10
+while a >= 1
+  puts a
+  a = a - 1
+end 
+
+#10から５までの整数を順番に表示する
+a = 10
+while a >= 5
+  puts a
+  a = a - 1 
+end
+
+#10から6までの整数を順番に並べる
+a = 10
+while a > 5
+  puts a
+  a = a - 1
+end
